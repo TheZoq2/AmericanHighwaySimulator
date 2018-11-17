4dc0d674b31a93c6f79d9cdd456a0738b1b6e018
TheZoq2 <frans.skarman@gmail.com>
2018-11-17 14:50:18 +0100

    Increase road speed

--- consts.hpp
+++ consts.hpp
@@ -15,7 +15,7 @@ const int CAR_DESPAWN_Y = WINDOW_HEIGHT - CAR_SPAWN_Y;
 const int CAR_SPAWN_MAX_OFFSET = -CAR_SPAWN_Y + CAR_DESPAWN_Y;
 const int CAR_SPEED = 100;
 const int CAR_SPEED_VARIATION = 100;
-const float ROAD_SPEED = 400;
+const float ROAD_SPEED = 800;
 
 const int PLAYER_WIDTH = 119;
 const int PLAYER_HEIGHT = 249;
